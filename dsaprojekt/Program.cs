8fd9f5b87ad833ad159d0c57ff984119a27bc60a
Martin <nordboe@live.dk>
2026-02-10 00:33:26 +0100

    Test loading of data from JSON files

--- dsaprojekt/Program.cs
+++ dsaprojekt/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace dsaprojekt
+﻿using System.Text.Json;
+
+namespace dsaprojekt
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string basePath = Path.Combine(Environment.CurrentDirectory, "Data");
+            string[] jsonFiles = { "notSorted.json", "reverseSorted.json", "sorted.json" };
+
+            string filePath = Path.Combine(basePath, jsonFiles[0]);
+            string jsonContent = File.ReadAllText(filePath);
+
+            //Console.WriteLine(jsonContent);
+
+
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            var data = JsonSerializer.Deserialize<JsonData>(jsonContent);
+
+            if(data != null)
+            {
+                foreach(var item in data.values)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
+
     }
+
+	public class JsonData
+	{
+        public int[] values { get; set; }
+	}
 }
