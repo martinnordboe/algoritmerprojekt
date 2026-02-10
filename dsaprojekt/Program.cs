e54e722c279a3fe67295060f3c58a085b6eed964
Martin <nordboe@live.dk>
2026-02-11 00:27:05 +0100

    Created basic ExportJsonData class to add JSON write method for results

--- dsaprojekt/Program.cs
+++ dsaprojekt/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System.Net.Http.Json;
 using System.Reflection.Metadata;
 using System.Text.Json;
+using dsaprojekt.Graphs;
 
 namespace dsaprojekt
 {
     internal class Program
     {
-        static string basePath = Path.Combine(Environment.CurrentDirectory, "Data");
-        static string[] jsonFiles = { "notSorted.json", "reverseSorted.json", "sorted.json" };
+        static string baseDataPath = Path.Combine(AppContext.BaseDirectory, "Data");
+
+		static string baseOutputPath = Path.Combine(Environment.CurrentDirectory, "Output");
+		static string outputResultPath = Path.Combine(baseOutputPath, "results.json");
+
+		static string[] jsonFiles = { "notSorted.json", "reverseSorted.json", "sorted.json" };
         
 		static MyList<int> myListOfNotSortedJson = new MyList<int>();
 		static MyList<int> myListOfReverseSortedJson = new MyList<int>();
@@ -15,15 +20,29 @@ namespace dsaprojekt
 
 		static void Main(string[] args)
         {
+			PopulateMyList(myListOfNotSortedJson, Path.Combine(baseDataPath, "notSorted.json"));
+			PopulateMyList(myListOfReverseSortedJson, Path.Combine(baseDataPath, "reverseSorted.json"));
+			PopulateMyList(myListOfSortedJson, Path.Combine(baseDataPath, "sorted.json"));
 
-			PopulateMyList(myListOfNotSortedJson, Path.Combine(basePath, "notSorted.json"));
-			PopulateMyList(myListOfReverseSortedJson, Path.Combine(basePath, "reverseSorted.json"));
-			PopulateMyList(myListOfSortedJson, Path.Combine(basePath, "sorted.json"));
+			myListOfNotSortedJson.InsertionSort();
+			myListOfReverseSortedJson.BubbleSort();
+			myListOfSortedJson.QuickSort();
+
+			Console.WriteLine("\n\n");
+			Console.WriteLine("SORTED!");
+			Console.WriteLine("\n");
+			foreach (var item in myListOfNotSortedJson)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\n\n");
+
+			WriteJsonData();
 		}
 
-        static void PopulateMyList<T>(MyList<T> myList, string completeFilePathy)
+        static void PopulateMyList<T>(MyList<T> myList, string completeFilePath)
         {
-			string jsonContent = File.ReadAllText(completeFilePathy);
+			string jsonContent = File.ReadAllText(completeFilePath);
 
 			JsonSerializerOptions options = new JsonSerializerOptions();
 			var data = JsonSerializer.Deserialize<JsonData<T>>(jsonContent);
@@ -35,7 +54,37 @@ namespace dsaprojekt
 					myList.Add(item);
 				}
 			}
-			Console.WriteLine($"Contents of {completeFilePathy} have been added to list");
+			Console.WriteLine($"Contents of {completeFilePath} have been added to list");
+		}
+
+		static T[] DepopulateMyList<T>(MyList<T> myList)
+		{
+			T[] values = new T[myList.Count];
+			for(int i = 0;  i < myList.Count; i++)
+			{
+				values[i] = myList[i];
+			}
+			return values;
+		}
+
+		static void WriteJsonData()
+		{
+			List<ExportJsonData<int>> results = new List<ExportJsonData<int>>()
+			{
+				new ExportJsonData<int> { fileName = "notSorted.json", values = DepopulateMyList(myListOfNotSortedJson) },
+				new ExportJsonData<int> { fileName = "reverseSorted.json", values = DepopulateMyList(myListOfReverseSortedJson) },
+				new ExportJsonData<int> { fileName = "sorted.json", values = DepopulateMyList(myListOfSortedJson) }
+			};
+
+			JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+			var jsonContent = JsonSerializer.Serialize(results, options);
+
+			File.WriteAllText(outputResultPath, jsonContent);
+		}
+
+		static void WriteTextFile()
+		{
+			//
 		}
 	}
 
@@ -43,4 +92,11 @@ namespace dsaprojekt
 	{
 		public T[] values { get; set; }
 	}
+
+    public class ExportJsonData<T>
+    {
+		public string fileName { get ; set; }
+
+        public T[] values { get; set; }
+    }
 }
