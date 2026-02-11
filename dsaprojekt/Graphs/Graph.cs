b61f0d77229bbb2e68d83c41a4e4258103b60706
Martin <nordboe@live.dk>
2026-02-11 02:12:29 +0100

    Switched from List to Dictionary in collection of nodes in Graph class, to find nodes based on arguments used as keys

--- dsaprojekt/Graphs/Graph.cs
+++ dsaprojekt/Graphs/Graph.cs
@@ -6,22 +6,31 @@ namespace dsaprojekt.Graphs
 {
 	public class Graph<T>
 	{
-		private List<Node<T>> nodes;
+		private Dictionary<T, Node<T>> nodes;
 
 		public Graph()
 		{
-			nodes = new List<Node<T>>();
+			nodes = new Dictionary<T, Node<T>>();
 		}
 
 		public void AddNode(T value)
 		{
-			nodes.Add(new Node<T>(value));
+			if (!nodes.ContainsKey(value))
+			{
+				nodes[value] = new Node<T>(value);
+			}
 		}
 
 		public void AddDirectedEdge(T from, T to)
 		{
 			// Hmmmm... får kun en to values og ikke to Nodes.
 			// Måske sammenlign med Data i Node elementer i listen?
+
+			// Fixet med Dictionary
+			if (nodes.ContainsKey(from) && nodes.ContainsKey(to))
+			{
+				nodes[from].AddEdge(nodes[to]);
+			}
 		}
 
 		public void AddEdge(T from, T to)
