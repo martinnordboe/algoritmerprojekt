2aaf52ba984a60523ae9f5df72bc28ec8014c30f
Martin <nordboe@live.dk>
2026-02-11 02:31:45 +0100

    Pseudo code for Bredth First Search and Depth First Search from project material

---
+++ dsaprojekt/Graphs/BFS.cs
@@ -0,0 +1,39 @@
+﻿namespace dsaprojekt.Graphs
+{
+	public class BFS
+	{
+		//procedure BFS(Graph, source):
+		//create a queue Q
+		//enqueue source onto Q
+		//mark source
+		//while Q is not empty:
+		//dequeue an item from Q into v
+		//for each edge e incident on v in Graph:
+		//let w be the other end of e
+		//if w is not marked:
+		//mark w
+		//enqueue w onto Q
+
+
+		//Forklaring af algoritmen
+
+		//1. Opret en queue og placer en fiktiv kant gående fra og til start
+
+		//2. Marker startnoden som besøgt
+
+		//3. Fjern og anvend den første kant fra jeres queue
+
+		//4. Hvis en slutnode ønskes fundet testes det om destinationen er
+		//slutnoden
+		//• Algoritmen afsluttes hvis dette er tilfældet
+		//• Stien findes ved at følge parentes fra destinationen tilbage til start
+
+		//5. Hvis slutnoden ikke er fundet tilføjes alle nabokanter med
+		//unvisited noder til jeres queue, hvis der ingen noder er
+		//backtrackes der.
+		//• Noderne markeres som visited.
+		//• Parent noden noteres
+
+		//6. Algoritmen gentages fra step 3
+	}
+}
---
+++ dsaprojekt/Graphs/DFS.cs
@@ -0,0 +1,40 @@
+﻿namespace dsaprojekt.Graphs
+{
+	public class DFS
+	{
+		// Pseudo kode fra undervisning
+
+		// procedure DFS-iterative(G,v):
+		// let S be a stack
+		// S.push(v)
+		//while S is not empty
+		//v ← S.pop()
+		//if v is not labeled as discovered:
+		//label v as discovered
+		//for all edges from v to w in G.adjacentEdges(v) do
+		//S.push(w)
+
+
+		//Forklaring af algoritmen
+
+		//1. Opret en stack og placer en fiktiv kant gående fra og til start
+
+		//2. Fjern og anvend top kanten fra jeres stack
+
+		//3. Noter parent til noden, start er parent til sig selv(kan også
+		//gøres i step 6, dette kan være lettere)
+
+		//4. Noter destinationsnoden som besøgt
+
+		//5. Hvis en slutnode ønskes fundet testes det om destinationen er
+		//slutnoden
+		//• Algoritmen afsluttes hvis dette er tilfældet
+		//• Stien findes ved at følge parentes fra destinationen tilbage til start
+
+		//6. Hvis slutnoden ikke er fundet tilføjes alle nabokanter med
+		//ubesøgte noder til stacken, hvis der ingen noder er backtrackes
+		//der
+
+		//7. Algoritmen gentages fra punkt 2 til målet er fundet
+	}
+}
