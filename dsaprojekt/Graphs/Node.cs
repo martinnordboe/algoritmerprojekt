b92f6b1db9cc42b90fde8eccffa73e9e40708474
Martin <nordboe@live.dk>
2026-02-11 01:38:52 +0100

    Created Node class for graph which is part two of the assignment

---
+++ dsaprojekt/Graphs/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dsaprojekt.Graphs
+{
+	public class Node<T>
+	{
+		public T Data { get; set; }
+		public List<Edge<T>> Edges { get; set; }
+
+		public Node(T data)
+		{
+			Data = data;
+			Edges = new List<Edge<T>>();
+		}
+
+		public void AddEdge(Node<T> other)
+		{
+			Edges.Add(new Edge<T>(this, other));
+		}
+	}
+}
